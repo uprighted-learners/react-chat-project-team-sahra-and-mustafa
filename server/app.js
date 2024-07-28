38b93499ae8ba9df6bc87e0b4fad2396f3d78a4f
IAmHim89 <computertech0418@gmail.com>
2024-07-28 16:32:30 -0400

    local server set up!

--- server/app.js
+++ server/app.js
@@ -1,2 +1,45 @@
-console.log("Can you see this?");
-console.log("I think i got it");
+//?env initialization
+require("dotenv").config();
+
+//!express initalization
+const express = require("express");
+
+//!variable holding our express call
+const app = express();
+
+//! variable for importing json
+app.use(express.json());
+
+//userController variable
+const userController = require("../server/controllers/userController");
+
+//userController
+//app.use("/user", userController);
+
+//!mongoose initialization
+const mongoose = require("mongoose");
+
+//!connection to url variable
+const MongoDB = process.env.MONGO_URL + process.env.MONGO_NAME;
+
+//!store to connection status
+mongoose.connect(MongoDB);
+
+//!store to connection status
+const db = mongoose.connection;
+
+//!.env port local host Variable
+const PORT = process.env.OURPORT || 5000;
+
+// Mongoose Connection callback
+db.once("open", () => {
+  console.log("*".repeat(30));
+  console.log(`Your are connected to \n ${MongoDB}`);
+  console.log("*".repeat(30));
+});
+
+//LocalHost Browser connection
+app.listen(PORT, () => {
+  console.log("*".repeat(30));
+  console.log(`Port ***${PORT}*** is up and running!`);
+});
