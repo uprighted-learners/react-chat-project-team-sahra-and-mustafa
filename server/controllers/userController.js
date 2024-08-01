d361cc21df99302374735522454ae2fc8278ef25
sahraomar <sahraaliwrites@gmail.com>
2024-08-01 14:14:04 -0400

    added user controller.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,18 +2,97 @@
 const router = require("express").Router();
 //! User variable holding our User model
 const User = require("../models/user_model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post("create", (req, res) => {
+//creating new user/hashing password
+
+// Creating a new user and hashing the password
+router.post("/create", async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create a new user instance with the hashed password
     const user = new User({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
-      password: req.body.email,
+      password: hashedPassword,
     });
+
+    //saving the user to database
+    const savedUser = await user.save();
+    res.status(201).json(savedUser);
   } catch (err) {
     res.status(500).json({
       Error: err.message,
+    });
+  }
+});
+
+//user log in auth
+router.post("/login", async (req, res) => {
+  try {
+    //find user by email
+    const user = await User.findOne({ email: req.body.email });
+    if (!user)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    // validate password
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    // JWT token
+    const token = jwt.sign(
+      { _id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).json({
+      Error: err.message,
+    });
+  }
+});
+
+// updating user info
+router.put("/:userId", async (req, res) => {
+  try {
+    const updatedUser = await User.findOneIdAndUpdate(
+      req.params.userId,
+      { $set: req.body },
+      { new: true }
+    );
+    if (!updatedUser)
+      return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json({
+      Error: err.message,
+    });
+  }
+});
+
+// delete a user which admin can do
+router.delete("/:userId", async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.userId);
+
+    if (!deletedUser)
+      return res.status(404).json({ message: "User not found" });
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (err) {
+    res.status(500).json({
+      Errror: err.message,
     });
   }
 });
