d05781d52e0209b58021aa289679da76fd2f44e1
IAmHim89 <computertech0418@gmail.com>
2024-08-12 18:10:41 -0400

    signup form created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,49 @@
+import SignUp from './Components/SignUp'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const {formState, setFormState} = useState(false)
+  
 
+ 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+
+        < div className='flex_container'>
+          <div className='header'>
+            <h3>Upright Educations</h3>
+            <h2>Sahra & Mustafa's</h2>
+            <h3>Application</h3>
+          </div>
+          <div className='form_container'>
+          <form className=' form_border'>
+            <div className='signup_text'>
+              <h4>SignUp Form</h4>
+            </div>
+         
+          
+          <input className='firstName' placeholder='FirstName' required />
+          <input className='lastName' placeholder='LastName' required/>
+          <input className='email' placeholder='Email' required/>
+          <input className='password' placeholder='Password' minLength={5} maxLength={12} required/>
+          
+
+          <button className='signup_btn'>
+              SignUp
+            </button>
+
+         </form>
+        
+            
+           
+          </div>
+        </div>
+      </main>
+   
+      
     </>
   )
 }
---
+++ client/src/Components/SignUp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Css_files/SignUpForm.css"
+
+
+const SignUp = () => {
+  
+    return(
+     <main className=''>
+       
+        
+       
+     </main>
+    ) 
+  
+  
+}
+
+export default SignUp
