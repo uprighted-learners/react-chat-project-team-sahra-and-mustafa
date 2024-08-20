8e8d1280702df80a9909d13df5e2b49416fff660
sahraomar <sahraaliwrites@gmail.com>
2024-08-19 20:53:05 -0400

    updated deleted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,70 +1,75 @@
 //! Import Area
-import Auth from './Components/Auth'
-import Messages from './Components/Messages'
+import Auth from "./Components/Auth";
+import Rooms from "./Components/Rooms";
+import Room from "./Components/Room";
+import { useState, useEffect } from "react";
 
-import { useState, useEffect } from 'react'
-
-import './App.css'
+import "./App.css";
 
 //! App function
 function App() {
-const [personalToken, setPersonalToken] = useState("")
-  
-//! useEffect for storing token in local storage
-useEffect(() => {
-  let token = localStorage.getItem("myToken")
-  if(token) {
-    setPersonalToken(token)
-  }
-}, [])
+  const [personalToken, setPersonalToken] = useState("");
 
- //! token callback update
- const updateToken = (token) => {
-  console.log("Token has been Updated!");
-  localStorage.setItem("myToken", token)
-  setPersonalToken(token)
+  //! useEffect for storing token in local storage
+  useEffect(() => {
+    let token = localStorage.getItem("myToken");
+    if (token) {
+      setPersonalToken(token);
+    }
+  }, []);
 
- }
+  //! token callback update
+  const updateToken = (token) => {
+    console.log("Token has been Updated!");
+    localStorage.setItem("myToken", token);
+    setPersonalToken(token);
+  };
 
   //! Remove Token callback
-  const removeToken = () =>{
+  const removeToken = () => {
     console.log("Token removed");
-    setPersonalToken("")
-    localStorage.clear()
-  }
-  
+    setPersonalToken("");
+    localStorage.clear();
+  };
+
   return (
     <>
-    <header>
+      <header>
         {/* Header/Title of Project*/}
-        < div className='flex_container'>
-          <div className='header'>
+        <div className="flex_container">
+          <div className="header">
             <h3>Upright Education</h3>
             <h2>Sahra & Mustafa's</h2>
             <h3>Application</h3>
           </div>
         </div>
-    </header>
+      </header>
 
       <main>
-        {/* Log Out tenary operator & button onclick event 
-          */}
-          {  !personalToken ? <Auth updateToken={updateToken}/> :   <button style={{position: "absolute", top: 0, right: 0, margin: "1em", backgroundColor: "orangered"}} onClick={() =>  removeToken()}>Logout</button>
-                         
-          }
+        {/* Log Out tenary operator & button onclick event
+         */}
+        {!personalToken ? (
+          <Auth updateToken={updateToken} />
+        ) : (
+          <button
+            style={{
+              position: "absolute",
+              top: 0,
+              right: 0,
+              margin: "1em",
+              backgroundColor: "orangered",
+            }}
+            onClick={() => removeToken()}
+          >
+            Logout
+          </button>
+        )}
 
         {/*
-            */}
-            <Messages/>
-
-         
-
-                
+         */}
       </main>
-   
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5174"],
+    origin: ["http://localhost:5173"],
   })
 );
 
